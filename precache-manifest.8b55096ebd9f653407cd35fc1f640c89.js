a4ae8b745efd23fd4e13879b838b459f4a05ef74
Asane <asane@AsanenoMacBook-Air.local>
2020-01-19 21:12:11 +0900

    Updates

---
+++ precache-manifest.8b55096ebd9f653407cd35fc1f640c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa63cd8f4070edb380114dd0dc02a3dc",
+    "url": "/monsterrelodex/index.html"
+  },
+  {
+    "revision": "4448a4b243f33b9f6f77",
+    "url": "/monsterrelodex/static/css/main.7ffef425.chunk.css"
+  },
+  {
+    "revision": "d706f28003964d78c4b2",
+    "url": "/monsterrelodex/static/js/2.1339efe9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsterrelodex/static/js/2.1339efe9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4448a4b243f33b9f6f77",
+    "url": "/monsterrelodex/static/js/main.e496c589.chunk.js"
+  },
+  {
+    "revision": "38fa177cf8ee6527fe30",
+    "url": "/monsterrelodex/static/js/runtime-main.ca1a51b6.js"
+  }
+]);
